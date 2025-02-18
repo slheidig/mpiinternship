e6203303b93751e811691320bed320b5d07260fe
Sophie - Luise Heidig <sophie@MacganLeFay.fritz.box>
2025-02-18 16:10:43 +0100

    initial phylogeny for aligment

--- bin/run_analysis.sh
+++ bin/run_analysis.sh
@@ -1,12 +1,8 @@
 
 
 mkdir -p andi
-
 andi --file-of-filenames=input_genomes_andi.txt --join --model=Kimura > andi/andi_distances.phy
 
 Rscript bin/reformat_andi.R andi/andi_distances.phy 48
-
 fastme -i andi/andi_distances_formated.phy -q
-
-
-Rscript bin/plot_ree.R
+Rscript bin/plot_tree.R
